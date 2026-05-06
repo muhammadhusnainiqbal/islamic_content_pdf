865f0528393c6b687342f8509feed7f4bf14c90c
shujahusnain <shujahusnainxi@gmail.com>
2026-05-06 21:28:36 +0500

    added google test app id and ad unit as a comment in example files

--- lib/secrets/secrets.example.dart
+++ lib/secrets/secrets.example.dart
@@ -1,4 +1,6 @@
 class Secrets {
+  // ca-app-pub-3940256099942544/6300978111 is test ad unit ID provided by Google for testing purposes. Replace it with your actual ad unit ID when deploying the app.
+
   static const String surahYaseenBannerUnitId = 'ca-app-pub-xxxxx/xxxxxxx';
 
   static const String surahRehmanBannerUnitId = 'ca-app-pub-xxxxx/xxxxxxx';
